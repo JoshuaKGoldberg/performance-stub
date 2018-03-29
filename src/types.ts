96ea61f2e921737cfa0590c487c4b147f5c743cb
Josh Goldberg <josh@fullscreenmario.com>
2018-03-28 17:13:59 -0700

    Added src/types.ts containing perf types

---
+++ src/types.ts
@@ -0,0 +1,56 @@
+// These types are copied from lib.dom.d.ts.
+// TypeScript projects consuming this library might not have "dom" in their lib types.
+
+interface Performance {
+    readonly navigation: PerformanceNavigation;
+    readonly timing: PerformanceTiming;
+    clearMarks(markName?: string): void;
+    clearMeasures(measureName?: string): void;
+    clearResourceTimings(): void;
+    getEntries(): any;
+    getEntriesByName(name: string, entryType?: string): any;
+    getEntriesByType(entryType: string): any;
+    getMarks(markName?: string): any;
+    getMeasures(measureName?: string): any;
+    mark(markName: string): void;
+    measure(measureName: string, startMarkName?: string, endMarkName?: string): void;
+    now(): number;
+    setResourceTimingBufferSize(maxSize: number): void;
+    toJSON(): any;
+}
+
+interface PerformanceNavigation {
+    readonly redirectCount: number;
+    readonly type: number;
+    toJSON(): any;
+    readonly TYPE_BACK_FORWARD: number;
+    readonly TYPE_NAVIGATE: number;
+    readonly TYPE_RELOAD: number;
+    readonly TYPE_RESERVED: number;
+}
+
+interface PerformanceTiming {
+    readonly connectEnd: number;
+    readonly connectStart: number;
+    readonly domComplete: number;
+    readonly domContentLoadedEventEnd: number;
+    readonly domContentLoadedEventStart: number;
+    readonly domInteractive: number;
+    readonly domLoading: number;
+    readonly domainLookupEnd: number;
+    readonly domainLookupStart: number;
+    readonly fetchStart: number;
+    readonly loadEventEnd: number;
+    readonly loadEventStart: number;
+    readonly msFirstPaint: number;
+    readonly navigationStart: number;
+    readonly redirectEnd: number;
+    readonly redirectStart: number;
+    readonly requestStart: number;
+    readonly responseEnd: number;
+    readonly responseStart: number;
+    readonly secureConnectionStart: number;
+    readonly unloadEventEnd: number;
+    readonly unloadEventStart: number;
+    toJSON(): any;
+}
